440d4959619a43576a9f5f2b6a39e3072eb20ed7
John Leavell <johnleavell@gmail.com>
2021-06-26 04:48:00 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,107 @@
+# Perform a constant expression evaluation
+#3
+#assing constant 3 to bare-word variable triangle_sides
+#'triangle_sides'
+
+# from the three dog night song: 'joy to the world (jermiaah was a bullforg)'
+
+#puts "joy to the world"
+#puts "all the boys and girls"
+#puts "joy to the fishes in the deep blue sea"
+#puts "joy to you and me"
+
+
+# name = "Byron"
+#name = "Luca"
+
+#puts "we're sorry, but per health inspector's rules, #{name}
+#is not allowed in the store."
+
+
+#run_code_inside = true
+#puts "Code bdfore if...end"
+#if run_code_inside
+  #puts "code inside"
+#end
+#puts "Code after if...end"
+
+
+#chance_of_rain = 1
+#puts "Let's go outside!"
+#if chance_of_rain > 0.5
+#  puts "Pack an umbrella!"
+#else
+#  puts "Enjoy the fine day!"
+#end
+#puts "Oh, and always sunscreen!"
+
+#chance_of_rain = -23
+#if chance_of_rain <= 0.25
+#  puts " Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#  puts "Pack an umbrella!"
+#else
+#  puts "Stay home and read Hegel."
+#end
+
+
+#puts "You konw what year it is??"
+#this_year = 2019
+#puts "Hey, it's 2019!" if this_year == 2019
+
+#this_year = 2019
+#puts Time.now.year unless this_year == 2018
+
+#name = "The White Rabbit"
+
+#if name == "Alice"
+#  puts "Hello, Alice!"
+#elsif name == "The White Rabbit"
+#  puts "Don't be late, White Rabbit"
+#elsif name == "The Mad Hatter"
+#  puts "Welcome to the tea party, Mad Hatter"
+#elsif name == "The Queen of Hearts"
+#  puts "Please don't chop off my head!"
+#else
+#  puts "Whooo are you?"
+#end
+
+#name = "Bob"
+
+#case name
+
+#  when "Alice"
+    # translated: when name == "Alice"
+#    puts "Hello, Alice!"
+#  when "The White Rabbit"
+#    puts "Don't be late, White Rabbit"
+#  when "The Mad Hatter"
+#    puts "Welcome to the tea party, Mad Hatter"
+#  when "The Queen of Hearts"
+#    puts "Please don't chop off my head!"
+#  else
+#    puts "Whooo are you?"
+#  end
+ 
+#greeting = "friendly_greeting"
+
+#case greeting
+#  when "unfriendly_greeting"
+#    puts "What do you want!?"
+#  when "friendly_greeting"
+#    puts "Hi! How are you?"
+#  end
+  
+ 
+ 
+current_weather = "raining"
+ 
+case current_weather
+  when "sunny"
+    puts "grab some sunscreen!"
+  when "raining"
+    puts "grab an umbrella"
+  when "snowing"
+    puts "bundle up"
+end
+  
